637ed0f3140d35e8160651804ef41efe14430c56
davidddeveloper <programingdonebydavid@gmail.com>
2024-09-18 12:53:21 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized')
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+        - 401 unauthorized
+    """
+    abort(401)
