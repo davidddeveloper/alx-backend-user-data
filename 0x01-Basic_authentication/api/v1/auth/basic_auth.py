13826244fd7c7fb81d3e60cfd3a2bd081a5195d6
davidddeveloper <programingdonebydavid@gmail.com>
2024-09-18 16:28:29 +0000

    basic auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ basic_auth.py: creates basic authentication """
+from api.v1.auth.auth import Auth
 
 
-class BasicAuth:
+class BasicAuth(Auth):
     """ Representas a basic authentication """
     pass
