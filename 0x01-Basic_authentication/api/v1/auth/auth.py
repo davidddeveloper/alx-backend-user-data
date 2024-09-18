aae77fdcd18d0d249de5054d7039ecf71a29e467
davidddeveloper <programingdonebydavid@gmail.com>
2024-09-18 13:48:00 +0000

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,8 +2,8 @@
 """
     template for all authentication system
 """
-# from typing import List
-# from typing import TypeVar
+from typing import List
+from typing import TypeVar
 
 
 class Auth:
@@ -11,10 +11,13 @@ class Auth:
         Represent authentication system
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ true if path not in excluded_paths """
         return False
 
     def authorization_header(self, request=None) -> str:
+        """ validates all request """
         return request
 
     def current_user(self, request=None) -> TypeVar('User'):
+        """ return the current user """
         return None
