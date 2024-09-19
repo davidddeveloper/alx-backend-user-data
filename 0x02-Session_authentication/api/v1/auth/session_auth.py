57573ced75a6aed793016e82b0d140fa9341b3c2
davidddeveloper <programingdonebydavid@gmail.com>
2024-09-19 12:42:35 +0000

    Empty session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ session_auth.py: Implements Session Authentication """
 from api.v1.auth.auth import Auth
 
+
 class SessionAuth(Auth):
     """ Represents a session authentication """
     pass
