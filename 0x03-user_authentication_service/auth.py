d0087e128ed234fab020f9c78bb0648229b6575f
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-24 13:16:54 +0000

    generate a password hash

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+    module: to create auth
+"""
+import bcrypt
+
+
+def _hash_password(password):
+    """
+        Generate a password hash
+    """
+    return bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt(10))
