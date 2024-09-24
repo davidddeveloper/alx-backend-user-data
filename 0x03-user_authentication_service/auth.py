bbc39b90133b96c19cef8137b463ffca7839b3dd
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-24 15:12:44 +0000

    destroys a session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -95,7 +95,7 @@ class Auth:
             destroy session
         """
         try:
-            user = self._db.find_user_by(**{"user_id": user_id})
+            user = self._db.find_user_by(**{"id": user_id})
 
         except NoResultFound:
             pass
