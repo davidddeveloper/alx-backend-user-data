f63ee49b4a9d339c87d99e46747657e3408ade0a
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-24 14:28:41 +0000

    validate login credentials

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -36,3 +36,19 @@ class Auth:
         hashed_password = _hash_password(password)
         user = self._db.add_user(email, hashed_password)
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+            valid login
+        """
+        try:
+            user = self._db.find_user_by(**{"email": email})
+
+        except NoResultFound:
+            return False
+        else:
+            if bcrypt.checkpw(
+                    password.encode('utf8'), user.hashed_password):
+                return True
+            else:
+                return False
