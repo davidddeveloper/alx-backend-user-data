52e2bc3d5f648cea18ac3c8a952506ea99545c86
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-26 11:27:02 +0000

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -54,7 +54,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id: str, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """
             Update a user model
         """
