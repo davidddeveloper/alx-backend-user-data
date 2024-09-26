2c3c766b87954a1aa5c73eef671941762dbe3500
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-26 11:05:49 +0000

    generate uuids

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -16,7 +16,7 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt(10))
 
 
-def _generate_uuid(self) -> str:
+def _generate_uuid() -> str:
     """
         Generate uuids
     """
