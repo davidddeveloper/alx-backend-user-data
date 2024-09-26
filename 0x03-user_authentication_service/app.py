1ace0d9014dcd96d5487ecd9df407e58f0b710d0
David Conteh <142942999+davidddeveloper@users.noreply.github.com>
2024-09-26 11:39:06 +0000

    logout

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -57,8 +57,6 @@ def login():
 @app.route("/sessions", methods=['DELETE'])
 def logout():
     session_id = request.cookies.get('session_id')
-    if not session_id:
-        abort(403)
 
     user = AUTH.get_user_from_session_id(session_id)
 
