e2061227d6ae67418edace09bf8cbabdefe397b9
davidddeveloper <programmingdonebydavid@gmail.com>
2024-11-16 14:46:53 +0000

    obfuscated the field

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+"""
+    filtered_logger.py
+    Module that filters a log file
+"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """
+        Returns the log message obfuscated
+        Args:
+            - fields: a list of strings representing all fields to obfuscate
+            - redaction: a string representing by what the field will be obfuscated
+            - message: a string representing the log line
+            - separator: a string representing by which character is separating all fields in the log line
+    """
+    for field in fields:
+        message = re.sub(
+            r'(?<={}=)[^{}]*'.format(field, separator),
+            redaction, message)
+    return message
