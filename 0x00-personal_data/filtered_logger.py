28149585caf3ba96218d7b5263c8a6642c9d66b7
davidddeveloper <programmingdonebydavid@gmail.com>
2024-11-16 15:00:42 +0000

    pycodestyle validated

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,14 +6,21 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(
+        fields: List[str],
+        redaction: str,
+        message: str,
+        separator: str) -> str:
     """
         Returns the log message obfuscated
         Args:
             - fields: a list of strings representing all fields to obfuscate
-            - redaction: a string representing by what the field will be obfuscated
+            - redaction: a string representing by what the field will be
+                obfuscated
             - message: a string representing the log line
-            - separator: a string representing by which character is separating all fields in the log line
+            - separator: a string representing by which character is separating
+                all fields in the log line
     """
     for field in fields:
         message = re.sub(
